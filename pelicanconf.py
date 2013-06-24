0217c856026f59515a0ff522253a5c2e9e5194d0
Markus Hubig <mhubig@gmail.com>
2013-06-24 18:32:54 +0200

    Adds a CNAME file.
    Signed-off-by: Markus Hubig <mhubig@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,18 +5,14 @@ from __future__ import unicode_literals
 AUTHOR = u'Markus Hubig'
 AUTHOR_EMAIL = u'mhubig@gmail.com'
 SITENAME = u'admin punk and python junk ...'
-SITEURL = u'http://mhubig.fritz.box:8000'
+SITEURL = u'http://localhost:8000'
 
 TIMEZONE = u'Europe/Paris'
 DEFAULT_LANG = u'en'
 DEFAULT_PAGINATION = 10
 
 THEME = u"/Users/markus/Development/pelican-octopress-theme"
-
-# GITHUB_USER = u'mhubig'
-# GITHUB_REPO_COUNT = 5
-# GITHUB_SKIP_FORK = True
-# GITHUB_SHOW_USER_LINK = False
+FILES_TO_COPY = (('extra/CNAME', 'CNAME'),)
 
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -34,16 +30,4 @@ LINKS =  (('IMKO GmbH', 'http://imko.de'),
 SOCIAL = (('twitter', 'http://twitter.com/mhubig'),
           ('bitbucket', 'http://bitbucket.org/mhubig'),
           ('github', 'http://github.com/mhubig'),)
-
-# global metadata to all the contents
-#DEFAULT_METADATA = (('administration', 'linux'),
-#                    ('python',          'punk'),)
-
-# path-specific metadata
-EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
-    }
-
-# static paths will be copied without parsing their contents
-STATIC_PATHS = ['extra/robots.txt',]
 
